7b651143fa54f38e589a004aea39637485703bc3
Marc-André tétrault <marc-andre.tetrault@usherbrooke.ca>
2026-04-23 04:47:24 -0400

    added pdk variables, pointing to installed symlinks for tool

--- sourceme.bash
+++ sourceme.bash
@@ -3,3 +3,5 @@
 export OPENRAM_HOME="$HOME/saba/OpenRAM/compiler"
 export OPENRAM_TECH="$HOME/saba/OpenRAM/technology"
 export PYTHONPATH=$PYTHONPATH:$OPENRAM_HOME
+export PDK_ROOT=$HOME/saba/OpenRAM/
+export PDK=sky130A
